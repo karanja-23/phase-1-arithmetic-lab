a0f0ebb879f46b37f75fb103507e109ac58ec7a6
Hosea <karanja.mungai23@gmail.com>
2024-09-23 17:35:34 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 12;
+let nums = [20,17,8,5,4];
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() +1);
+
+const mod = num3 % num4;
+
+const max = Math.max(...nums);
